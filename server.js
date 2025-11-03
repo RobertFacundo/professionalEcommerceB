a335ed1667313aed1c4f7d56757f3eee103d5d92
Facundo Robert <robert.facundo@hotmail.com>
2025-11-03 12:51:14 -0300

    initial infrastructure of MERN ecommerce

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json('Backend running')
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
